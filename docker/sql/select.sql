6304dc32946af563dbc1da3d718bfe745dfc4b0e
jin9358 <leejh9358@naver.com>
2025-05-23 09:12:36 +0900

    feat: sql 기초

--- docker/sql/select.sql
+++ docker/sql/select.sql
@@ -15,3 +15,35 @@ select * from (select 10 + 20 as number2, 40 as number3) as n_tb;
 */
 select 30, 40 union select 50, '가';
 select 10, 20 union all select 10, 20;
+
+/*
+	JOIN(테이블 합치기)
+    SELECT * FROM 테이블1 JOIN 테이블2;
+*/
+select * from student_tb as st1 join student_tb as st2;
+select * from 
+student_tb st1 
+left outer join 
+student_tb st2 on(st2.student_id = st1.student_id + 1)
+left outer join
+student_tb st3 on(st3.student_id = st2.student_id + 1);
+
+select 
+	* 
+from 
+	student_tb st1 
+	inner join student_tb st2 on(st2.student_id = st1.student_id + 1)
+	left outer join student_tb st3 on(st3.student_id = st2.student_id + 1);
+    
+/*
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (j == i) {
+				테이블 붙이기
+			}
+		}
+	}
+    
+    int a = 10;
+    if (10 == a) {}
+*/
